6f0200bf4a56c23b059a85d7f4bb69acd4bcafee
Dmitry Volovod <dimazoll@shri2018.yaconnect.com>
2018-10-15 01:43:17 +0300

    add 404 http error handler

--- src/handlers/error.js
+++ src/handlers/error.js
@@ -1,9 +1,7 @@
-function errorHandler(error, request, response, next) {
-  return response.status(error.status || 500).json({
-    error: {
-      message: error.message || "Oops! Something went wrong."
-    }
-  });
+function errorHandler(req, res, next) {
+  res
+    .status(404)
+    .send('<h1>Status: 404</h1><h2>Page not found</h2>');
 }
 
 module.exports = errorHandler;
